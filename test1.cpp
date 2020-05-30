ad947df67205a3f339b3a0b6d649557c1e32844e
Sanjeeev-K <sanjeevcannon@gmail.com>
2020-05-30 01:35:44 -0400

    After revert2

--- test1.cpp
+++ test1.cpp
@@ -2,17 +2,5 @@
 
 int main(){
     int data;
-    //line1
-    
-    //line4
-    //line 5
-    //line 6
-    //line7
-    //lin 7
-    //line 8
-    //line 9
-    //line 10
-    
-    //line11
-    //line 12
+    //test1
 }
