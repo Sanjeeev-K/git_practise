7a1e9222185cc6cd47e35e3d6b085bba42bc6445
Sanjeeev-K <sanjeevcannon@gmail.com>
2020-05-30 17:05:27 -0400

    Clean up

--- test1.cpp
+++ test1.cpp
@@ -2,14 +2,4 @@
 
 int main(){
     int data;
-    //test1
-    //test2
-    //test3
-    //test4 - local
-    //test5 - change in Web.test for rebase
-    //test5 - in branch testing_rebase1
-    //test6 - in branch testing_rebase1
-    //test7 - in branch testing_rebase2
-    //test8 - local. testing_rebase2
-    //test8 - random
 }
