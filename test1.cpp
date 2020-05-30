1dc837ca732c3213947a378dd1f7486f490ed11d
Sanjeeev-K <sanjeevcannon@gmail.com>
2020-05-30 16:57:22 -0400

    Random

--- test1.cpp
+++ test1.cpp
@@ -6,12 +6,9 @@ int main(){
     //test2
     //test3
     //test4 - local
-<<<<<<< HEAD
     //test5 - change in Web.test for rebase
-=======
     //test5 - in branch testing_rebase1
     //test6 - in branch testing_rebase1
     //test7 - in branch testing_rebase2
     //test8 - local. testing_rebase2
->>>>>>> testing_rebase2
 }
